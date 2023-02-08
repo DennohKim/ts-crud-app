ce33bbe5e79d2cdcb95293e64d4f961fb96ee097
DennohKim <kimathidennis38@gmail.com>
2023-02-08 22:15:24 +0300

    style: create default theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React, {FC, ReactElement} from 'react';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { customTheme } from './theme/customTheme';
 
 
 const App:FC = ():ReactElement => {
   return (
-    <>
+    <ThemeProvider theme={customTheme}>
+      <CssBaseline/>
       <h1>Chizaa</h1>
-    </>
+    </ThemeProvider>
   );
 };
 

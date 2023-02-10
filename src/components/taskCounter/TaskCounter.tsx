64b29a9a60a63cf99afbb7bf668aae3bb0f451d0
DennohKim <kimathidennis38@gmail.com>
2023-02-10 08:28:02 +0300

    feat: create task completion component

---
+++ src/components/taskCounter/TaskCounter.tsx
@@ -0,0 +1,57 @@
+import React, { FC, ReactElement } from 'react';
+import { Avatar, Box, Typography } from '@mui/material';
+import { ITaskCounter } from './interfaces/ITaskCounter';
+import { Status } from '../createTaskForm/enums/Status';
+import { emitCorrectBorderColors } from './helpers/emitCorrectBorderColors';
+import { emitCorrectLabel } from './helpers/emitCorrectLabel';
+import PropTypes from 'prop-types';
+
+export const TaskCounter: FC<ITaskCounter> = ({
+  count = 5,
+  status = Status.inProgress,
+}): ReactElement => {
+  return (
+    <>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Avatar
+          sx={{
+            backgroundColor: 'transparent',
+            border: '5px solid',
+            width: '96px',
+            height: '96px',
+            marginBottom: '16px',
+            borderColor: `${emitCorrectBorderColors(
+              status,
+            )}`,
+          }}
+        >
+          <Typography color="#ffffff" variant="h4">
+            {count}
+          </Typography>
+        </Avatar>
+        <Typography
+          color="#ffffff"
+          fontWeight="bold"
+          fontSize="20px"
+          variant="h5"
+        >
+          {emitCorrectLabel(status)}
+        </Typography>
+      </Box>
+    </>
+  );
+};
+
+TaskCounter.propTypes = {
+  count: PropTypes.number,
+  status: PropTypes.oneOf([
+    Status.todo,
+    Status.inProgress,
+    Status.completed,
+  ]),
+};
--- src/components/taskarea/Taskarea.tsx
+++ src/components/taskarea/Taskarea.tsx
@@ -1,10 +1,47 @@
-import { Grid } from '@mui/material';
+import { Grid, Box } from '@mui/material';
 import React, { FC, ReactElement } from 'react';
+import { format } from 'date-fns';
+import { TaskCounter } from '../taskCounter/TaskCounter';
 
 export const Taskarea: FC = (): ReactElement => {
   return (
     <Grid item md={8} px={4}>
-      <h2>Content Area</h2>
+      <Box mb={8} px={4}>
+        <h2>
+          Status Of Your Tasks As On{' '}
+          {format(new Date(), 'PPPP')}{' '}
+        </h2>
+      </Box>
+      <Grid
+        container
+        display="flex"
+        justifyContent="center"
+      >
+        <Grid
+          item
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-around"
+          alignItems="center"
+          md={10}
+          xs={12}
+          mb={8}
+        >
+          <TaskCounter/>
+          <TaskCounter/>
+          <TaskCounter/>
+        </Grid>
+        <Grid
+          item
+          display="flex"
+          flexDirection="column"
+          xs={10}
+          md={8}
+        >
+          <Box>Tasks Will Come Over Here</Box>
+          <Box>Tasks Will Come Over Here</Box>
+        </Grid>
+      </Grid>
     </Grid>
   );
 };

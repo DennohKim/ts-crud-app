4031b3eee4d17b4abfdce67e8e5697ffce11af41
DennohKim <kimathidennis38@gmail.com>
2023-02-11 10:06:21 +0300

    feat: intergrate typeORM

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,24 @@
 import express , {Express, Request, Response} from "express";
 import dotenv from "dotenv";
+import { DataSource } from 'typeorm'
 
 
 //instanciate express app
 
 const app: Express = express();
 dotenv.config();
+
+//Create a database connection
+
+export const AppDataSource = new DataSource({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB,
+    synchronize: true,
+})
 
 //Define server Portal
 const port = process.env.PORT;
@@ -15,5 +28,12 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Hello Chizaa');
 });
 
-//start listening on port 3000
-app.listen(port);
+//Initialize app once mysql is available
+AppDataSource.initialize().then(() => {
+  //start listening on port 3000
+  app.listen(port);
+  console.log("Data source initialised")
+}).catch((err) => {
+    console.error('Error during data source initialization',err)
+})
+
